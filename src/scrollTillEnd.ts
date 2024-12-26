52e5b2d754f771f1489ab734a12e67bf1d23c201
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-26 18:17:01 +0530

    refractor:  update scrollTillEnd to accept tweets array, improve post tracking, add logging, and optimize wait time

--- src/scrollTillEnd.ts
+++ src/scrollTillEnd.ts
@@ -1,13 +1,32 @@
 import type { Page } from "puppeteer";
+import type { ExtractedTweet } from "./types";
 
 export async function scrollTillEnd(
   page: Page,
-  currentPostCount: number,
+  extractedTweets: ExtractedTweet[],
   totalNumberOfPosts: number
 ) {
-  while (currentPostCount < totalNumberOfPosts) {
-    // const previousHeight = await page.evaluate("document.body.scrollHeight");
+  console.log("Called");
+  let previousPostCount = extractedTweets.length;
+  let sameCountIterations = 0;
+
+  while (extractedTweets.length < totalNumberOfPosts) {
     await page.evaluate("window.scrollTo(0,document.body.scrollHeight)");
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
+    // Check if post count hasn't changed
+    if (previousPostCount === extractedTweets.length) {
+      sameCountIterations++;
+      console.log(extractedTweets.length, "same count", sameCountIterations);
+      if (sameCountIterations >= 20) {
+        throw new Error(
+          "Failed to load more posts: Post count remained same for 20 iterations"
+        );
+      }
+    } else {
+      console.log("reset counter");
+      sameCountIterations = 0;
+      previousPostCount = extractedTweets.length;
+    }
   }
 }
